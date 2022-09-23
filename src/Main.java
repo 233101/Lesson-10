23cdee100e127c47e29f8da8d7982b11b1d57fbb
Abdykadyr Gaziev <abdyk@wsm21.REFORMENG.LOCAL>
2022-09-23 21:56:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+   /*     me("Java",
+                "Js",
+                "Python",
+                "Rubi",
+                "C++",
+                "C#");
+
+    } static void me(String...c){
+        for (String a : c) {
+            String b = "I LOVE ";
+            b+=" "+a;
+            System.out.println(b);
+        }*/
+
+
+
+
+        /**System.out.println(name.charAt(1));//берилген индекстеги символду кайтарып берет
+        String kesuu=name.substring(2);//берилген индекстен баштап созду кесип алат
+        System.out.println(kesuu);
+        System.out.println(name.contains("r"));//берилген символ создун ичинде ьарбы же жокпу текшерет-> true or false
+        System.out.println(name.toUpperCase());//баардык тамганы чог кылып салат
+        System.out.println(name.toLowerCase());//баардык тамгаын кичине кылып салат
+        System.out.println(soz.trim());//башындагы жана аягындагы боштуктарды алып салат
+        System.out.println(soz2.equals(soz));//2 созду бири бирине барабарбы деп текшерет
+        System.out.println(name);
+        System.out.println(name.replace('a','u'));//создун каалаган тамгасын жаны каалаган тамшага озгортсок болот
+        System.out.println(name.length());//создун узундугун чыгарат
+        System.out.println(name.concat(" Abdyldaev"));//2 созду бириктирет
+         System.out.println(name.indexOf("n"));//берилген символдун индексин кайтарып берет**/
+
+        /** Суроо: Консолдон соз сураган код жазыныз.
+         Метод тузунуз жана консолдон алган созунузду
+         методко аргумент катары бериниз.
+         Метод создун бардык тамгаларын чон кылсын.
+         Ашыкча боштуктарды жок кылсын.
+         Эгерде создо "А" деген тамга камтылса анда
+         созду а деген тамгага чейин кесип кайтарсын.
+         Эгерде "А" деген тамга камтыбаса анда "Жок"
+         деген созду кайтарсын    **/
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(word(s));
+
+    }
+    static String word (String s ){
+        String s1= s.toUpperCase();
+        s1.trim();
+       if (s1.contains("A")){
+           return s1.substring(s1.indexOf("A"));
+       }else {
+           return "NO";
+       }*/
+
+    }
+}
